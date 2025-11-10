787fe83ed2b7576763929cb536d05864b6f21072
Inigo Marin Garcia <inigomaringarcia@gmail.com>
2025-11-10 19:46:24 +0100

    fix: update image URL in omarchy-configure-gemini.sh script

--- omarchy-configure-gemini.sh
+++ omarchy-configure-gemini.sh
@@ -2,4 +2,4 @@
 
 
 # Call the omarchy-webapp-install script with the specified parameters
-omarchy-webapp-install "Gemini" "https://gemini.google.com" "gemini.png"
+omarchy-webapp-install "Gemini" "https://gemini.google.com" https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyixsXnYopHtRjXSxUVvj7OAH5Qw4mJsZGZ7hDQS9jOMAe5lqm0qnnO1VJRyTriXKHbdk&usqp=CAU
